5bd49d2c67cc5bd8d7a4e92b18c324b885d8943b
akhil90s <akhil.chawla2010@gmail.com>
2021-01-13 22:58:04 +0530

    Multi-Threading

--- src/ways/to/create/thread/UsingTheRunnableInterface.java
+++ src/ways/to/create/thread/UsingTheRunnableInterface.java
@@ -1,20 +1,26 @@
 package ways.to.create.thread;
 
-public class UsingTheRunnableInterface implements Runnable{
+public class UsingTheRunnableInterface implements Runnable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		// Runnable Interface Method 1
 		Thread t1 = new Thread(new UsingTheRunnableInterface());
 		t1.start();
 
+		// Runnable Interface Method 2
+		Runnable run = () -> {
+			System.out.println("Using Runnable Method Way 2");
+		};
+		Thread t2 = new Thread(run);
+		t2.start();
+		Thread.currentThread();
 	}
 
 	@Override
 	public void run() {
-		
-		System.out.println("Using Runnable");
-		
-	}
 
+		System.out.println("Using Runnable Method Way 1");
+
+	}
 }
